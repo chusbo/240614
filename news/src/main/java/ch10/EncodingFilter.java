72c6931b1acdb896dc477163c9eee3757db2191f
chusbo <67871203+chusbo@users.noreply.github.com>
2024-06-14 17:10:20 +0900

    240614-Academy-news

---
+++ news/src/main/java/ch10/EncodingFilter.java
@@ -0,0 +1,36 @@
+package ch10;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter("*.nhn")
+public class EncodingFilter implements Filter {
+
+    public EncodingFilter() {
+    }
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpReq = (HttpServletRequest)request;
+		
+		if(httpReq.getMethod().equalsIgnoreCase("PSOT")) {
+			request.setCharacterEncoding("utf-8");
+		}
+		chain.doFilter(request, response);
+}
+	
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
